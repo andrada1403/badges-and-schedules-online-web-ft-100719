60d2fb7c35e9d20a5a66669e5cfea4707df617f1
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-16 18:59:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do |name|
+    badges<< "Hello, my name is #{name}."
+  end
+  badges
+end
+
+def assign_rooms(attendees,room="")
+  room_assignments=[]
+  attendees.each do |name|
+    room_assignments<< "Hello, #{name}! You'll be assigned to #{room}!"
+  end
+  room_assignments
+end
