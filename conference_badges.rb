f5df5dc33c09da773df094640a6f6bdbcfc8131f
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-16 19:21:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(attendees)
   attendees.map.with_index do |name,index|
-    room_assignments<< "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
 end
